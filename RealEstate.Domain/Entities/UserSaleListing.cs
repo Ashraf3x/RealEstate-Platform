d96e28f6669b67b4ee59798cea4ade15037f45b2
Ahmed Ashraf <ahmedashrafmohamed112@gmail.com>
2026-03-03 11:50:29 +0200

    Add SaleListing, Contract, UserPropertyContract, UserSaleListing Entities

---
+++ RealEstate.Domain/Entities/UserSaleListing.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate.Domain.Entities
+{
+    internal class UserSaleListing
+    {
+        public int UserId { get; set; }
+        public int ListingId { get; set; }
+        public User User { get; set; }
+        public SaleListing SaleListing { get; set; }
+    }
+}
