a1a4bbc33a90f898fac5e540f0d719b21ae276d7
Ahmed Ashraf <ahmedashrafmohamed112@gmail.com>
2026-03-04 23:39:37 +0200

    Add AppDbContext and GenericRepository

---
+++ RealEstate.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using RealEstate.Infrastructure.Data;
+
+namespace RealEstate.Infrastructure.Repositories
+{
+    public class GenericRepository<T> where T : class
+    {
+        AppDbContext context;
+
+        public GenericRepository(AppDbContext con)
+        {
+            context = con;
+        }
+
+        public List<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Set<T>().Update(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+        }
+    }
+}
