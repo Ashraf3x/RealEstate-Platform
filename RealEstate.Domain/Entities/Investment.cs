1c11303446adc54758fd3cf41e6ea20d3f4e4416
Ahmed Ashraf <ahmedashrafmohamed112@gmail.com>
2026-03-04 05:05:53 +0200

    Add Investment, Yield and Purchase Entities

---
+++ RealEstate.Domain/Entities/Investment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealEstate.Domain.Entities
+{
+    public class Investment
+    {
+        [Key]
+        public int InvestmentId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [ForeignKey("Property")]
+        public int PropertyId { get; set; }
+
+        public int ShareCount { get; set; }
+        public decimal OwnershipPercentage { get; set; }
+        public DateTime PurchasedAt { get; set; }
+
+        public User User { get; set; }
+        public Property Property { get; set; }
+    }
+}
