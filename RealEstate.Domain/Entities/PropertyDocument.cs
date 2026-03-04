2313576854585190660bc76f25e1c783884260ab
Ahmed Ashraf <ahmedashrafmohamed112@gmail.com>
2026-03-04 04:58:03 +0200

    Add Property and PropertyDocument Entities

---
+++ RealEstate.Domain/Entities/PropertyDocument.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealEstate.Domain.Entities
+{
+    public class PropertyDocument
+    {
+        [Key]
+        public int DocId { get; set; }
+
+        [ForeignKey("Property")]
+        public int PropertyId { get; set; }
+
+        [MaxLength(50)]
+        public string DocumentType { get; set; }
+
+        [MaxLength(255)]
+        public string FilePath { get; set; }
+
+        public DateTime UploadedAt { get; set; }
+
+        public Property Property { get; set; }
+    }
+}
