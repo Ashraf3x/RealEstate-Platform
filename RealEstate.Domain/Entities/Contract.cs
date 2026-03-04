be454ab2f31d55d23949c61880802c251ec55f1e
Ahmed Ashraf <ahmedashrafmohamed112@gmail.com>
2026-03-04 04:36:14 +0200

    Update Listings Entities

--- RealEstate.Domain/Entities/Contract.cs
+++ RealEstate.Domain/Entities/Contract.cs
@@ -3,9 +3,12 @@ namespace RealEstate.Domain.Entities
 {
     public class Contract
     {
+        [Key]
         public int ContractId { get; set; }
+
         [MaxLength(255)]
         public string FilePath { get; set; }
+
         public ICollection<UserPropertyContract> UserPropertyContracts { get; set; }
     }
 }
--- RealEstate.Domain/Entities/UserPropertyContract.cs
+++ RealEstate.Domain/Entities/UserPropertyContract.cs
@@ -1,10 +1,21 @@
-﻿namespace RealEstate.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealEstate.Domain.Entities
 {
     public class UserPropertyContract
     {
+        [Key, Column(Order = 0)]
+        [ForeignKey("User")]
         public int UserId { get; set; }
+
+        [Key, Column(Order = 1)]
+        [ForeignKey("Property")]
         public int PropertyId { get; set; }
+
+        [ForeignKey("Contract")]
         public int ContractId { get; set; }
+
         public User User { get; set; }
         public Property Property { get; set; }
         public Contract Contract { get; set; }
