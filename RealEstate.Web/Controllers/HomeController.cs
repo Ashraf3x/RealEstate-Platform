655352ff7b3741e302cec7cbc65a7ce5b04df1eb
sama717 <sama.b.farag.7@gmail.com>
2026-03-07 22:15:43 +0200

    Adjust Layout, Add AdminLayout, Add HomeController and Index

---
+++ RealEstate.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
