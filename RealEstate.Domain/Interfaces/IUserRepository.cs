579687f380d246b170c8711ccdf12c9de2dbbf46
Ahmed Ashraf <ahmedashrafmohamed112@gmail.com>
2026-03-07 18:52:15 +0200

    Add User Repository, Service, DTOs and Controller

---
+++ RealEstate.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RealEstate.Domain.Entities;
+
+namespace RealEstate.Domain.Interfaces
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        User GetByEmail(string email);
+        List<User> GetActiveUsers();
+    }
+}
---
+++ RealEstate.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstate.Domain.Entities;
+using RealEstate.Domain.Interfaces;
+using RealEstate.Infrastructure.Data;
+
+namespace RealEstate.Infrastructure.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        AppDbContext context;
+
+        public UserRepository(AppDbContext con) : base(con)
+        {
+            context = con;
+        }
+
+        public new List<User> GetAll()
+        {
+            return context.Users.Include(u => u.KycDocuments).Include(u => u.Wallet).ToList();
+        }
+
+        public User GetByEmail(string email)
+        {
+            return context.Users.FirstOrDefault(u => u.Email == email);
+        }
+
+        public List<User> GetActiveUsers()
+        {
+            return context.Users.Where(u => u.IsActive == true).ToList();
+        }
+    }
+}
